b436f8109af08c0322d911b7d80ad82e6654d4b1
Higor Henrique <higorhlg@hotmail.com>
2018-06-30 14:27:58 -0300

    correcoes vendas component

--- aplicacaoSupermercado/src/app/vendas/vendas.component.ts
+++ aplicacaoSupermercado/src/app/vendas/vendas.component.ts
@@ -2,21 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 import { Produto } from '../../model/produto.model';
 import { Venda } from '../../model/venda.model';
+import { Produtos } from '../data';
+import { equalSegments } from '@angular/router/src/url_tree';
 
 @Component({
   selector: 'app-vendas',
   templateUrl: './vendas.component.html',
   styleUrls: ['./vendas.component.css']
 })
+
 export class VendasComponent implements OnInit {
 
   venda: Venda = new Venda();
-  produtos: Array<Produto>;
+  produtos: Array<Produto> = Produtos;
 
-  atualizaValorVenda(){
+  nomeSelecionado: String;
+  atualiza(){
+    this.venda.setProduto(this.produtos.find(x => x.nome === this.nomeSelecionado))
     if(this.venda.getQuantidade() === undefined)
       this.venda.setQuantidade(1);
-    // this.venda.setValor(this.venda.getProduto().getPrecoVenda() * this.venda.getQuantidade());
+     this.venda.setValor(this.venda.getProduto().precoVenda * this.venda.getQuantidade());
 
   }
   constructor() { }
--- aplicacaoSupermercado/src/model/produto.model.ts
+++ aplicacaoSupermercado/src/model/produto.model.ts
@@ -4,63 +4,62 @@
 export class Produto{
 
 
-    private nome?:string;
-    private categoria?:string; 
-    private peso?:number;
-    private precoCusto?:number;
-    private precoVenda?:number;
-    private descricao?:string;
-    private barCode?:number;
+    nome?:string;
+    categoria?:string; 
+    peso?:number;
+    precoCusto?:number;
+    precoVenda?:number;
+    descricao?:string;
+    barCode?:number;
+    unidades?: number;
 
  
-    constructor(){
+  
 
-    }
-
-    public setNome(nome:string){
-        this.nome=nome;
-    }
-    public setCategoria(categoria:string){
-        this.categoria=categoria;
-    }
-    public setPeso (peso :number){
-        this.peso=peso;
-    }
-    public setPrecoCusto(precoCusto:number){
-        this.precoCusto=precoCusto;
-    }
-    public setPrecoVenda(precoVenda:number){
-        this.precoVenda=precoVenda;
-    }
-    public setDescricao(descricao:string){
-        this.descricao=descricao;
-    }
-    public setBarCode(barCode:number){
-        this.barCode=barCode;
-    }
+    // public setNome(nome:string){
+    //     this.nome=nome;
+    // }
+    // public setCategoria(categoria:string){
+    //     this.categoria=categoria;
+    // }
+    // public setPeso (peso :number){
+    //     this.peso=peso;
+    // }
+    // public setPrecoCusto(precoCusto:number){
+    //     this.precoCusto=precoCusto;
+    // }
+    // public setPrecoVenda(precoVenda:number){
+    //     this.precoVenda=precoVenda;
+    // }
+    // public setDescricao(descricao:string){
+    //     this.descricao=descricao;
+    // }
+    // public setBarCode(barCode:number){
+    //     this.barCode=barCode;
+    // }
 
 
-    public getNome():string{
-        return this.nome;
-    }
-    public getCategoria():string {
-        return this.categoria;
-    }
-    public getPeso ():number{
-        return this.peso;
-    }
-    public getPrecoCusto():number{
-        return this.precoCusto;
-    }
-    public getPrecoVenda():number{
-        return this.precoVenda;
-    }
-    public getDescricao():string{
-        return this.descricao;
-    }
-    public getBarCode():number{
-        return this.barCode;
-    }
+    // public getNome():string{
+    //     return this.nome;
+    // }
+    // public getCategoria():string {
+    //     return this.categoria;
+    // }
+    // public getPeso ():number{
+    //     return this.peso;
+    // }
+    // public getPrecoCusto():number{
+    //     return this.precoCusto;
+    // }
+    // public getPrecoVenda():number{
+    //     return this.precoVenda;
+    // }
+    // public getDescricao():string{
+    //     return this.descricao;
+    // }
+    // public getBarCode():number{
+    //     return this.barCode;
+    // }
 
 }
 
